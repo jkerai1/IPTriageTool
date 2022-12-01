600e3c6b63cca893ca04837332ccf3a621ce0c86
J/ <55988027+jkerai1@users.noreply.github.com>
2022-12-01 19:00:20 +0000

    Update PasteTriage.bat

--- PasteTriage.bat
+++ PasteTriage.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-paste > temp.txt
+paste > temp.txt #need paste installed see https://www.c3scripts.com/tutorials/msdos/paste.html
 set /p Ip=<temp.txt
 SET BROWSER=brave.exe
 START %BROWSER% -new-tab "https://www.virustotal.com/gui/ip-address/%Ip%/detection"
